a725d2fe48e098d3565ca10f04b11e1e69e40101
Eric Holscher <eric@ericholscher.com>
2008-11-11 02:55:41 -0600

    Cleaning up repo

--- test_utils/templatetags/utils.py
+++ test_utils/templatetags/utils.py
@@ -1,7 +1,3 @@
-
-from django.template import Node, Template
-from template_utils.nodes import ContextUpdatingNode
-#from test_utils.templatetags.utils import context_for_object
 from django import template 
 
 register = template.loader()
@@ -28,16 +24,4 @@ def context_for_object(token, Node):
     else:
         #raise template.TemplateSyntaxError, "%s: Fail" % bits[]
         print "ERROR"
-        
-        
-class FooNode():
-    def __init__(self, por, _as='default'):
-        print "Making Node: for:%s, as:%s" % (por, _as)
 
-    
-    
-    @register.tag
-    def map(parser, token):
-        return context_for_object(token, FooNode, func)
-
-    
