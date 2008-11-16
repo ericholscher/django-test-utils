bd7f32446824ef545a7a4606a93199c142645487
Eric Holscher <eric@ericholscher.com>
2008-11-15 18:03:49 -0600

    Added testmaker into test-utils, mostly re-written

---
+++ test_utils/management/commands/testmaker.py
@@ -0,0 +1,85 @@
+from django.core.management.base import AppCommand, CommandError
+from optparse import make_option
+import sys, logging, os, re
+from os import path
+from django.db.models import get_model, get_models, get_app
+from django.db.models.fields.related import ManyToManyField, ForeignKey
+from django.core import serializers
+from django.test.signals import template_rendered
+from django.conf import settings
+
+
+class Command(AppCommand):
+    option_list = AppCommand.option_list + (
+        make_option('-f', '--fixture', action='store_true', dest='fixture',
+            help='Pass -f to create a fixture for the data.'),
+        make_option('--format', default='json', dest='format',
+            help='Specifies the output serialization format for fixtures.'),
+        make_option('--addrport', action='store', dest='addrport', 
+            type='string', help='port number or ipaddr:port to run the server on'),
+    )
+
+    help = 'Runs the test server with the testmaker output enabled for a specific app'
+    args = '[app to test]'
+    
+    def handle_app(self, app, **options):
+
+        verbosity = int(options.get('verbosity', 1))
+        create_fixtures = options.get('fixture', False)
+        format = options.get('format', 'json')
+        addrport = options.get('addrport', '')
+        
+        app_name = app.__name__.split('.')[-2]
+        base_dir = path.dirname(app.__file__)
+        
+        #Figure out where to store data
+        if create_fixtures:
+            fixtures_dir = path.join(base_dir, 'fixtures')
+            fixture_file = path.join(fixtures_dir, '%s-testmaker.%s' % (app_name, format))
+            if not path.exists(fixture_dir):
+                os.mkdir(fixture_dir)
+                
+        tests_dir = path.join(base_dir, 'tests')
+        test_file = path.join(tests_dir, '%s-testmaker.py' % (app_name))
+        if not path.exists(tests_dir):
+            os.mkdir(tests_dir)
+        
+        if verbosity > 0:
+            print "Handling app '%s'" % app_name
+            print "Logging tests to %s" % test_file
+            if create_fixtures:
+                print "Logging fixtures to %s" % fixturefile
+        
+        logging.basicConfig(level=logging.CRITICAL,
+                            filename="/dev/null")
+        
+        log = logging.getLogger('testmaker')
+        log.setLevel(logging.INFO)
+        handler = logging.FileHandler(test_file, 'w')
+        handler.setFormatter(logging.Formatter('%(message)s'))
+        log.addHandler(handler)
+
+        log.info('from django.test import TestCase')
+        log.info('from django.test import Client')
+        log.info('c = Client()')
+        log.info('class Testmaker(TestCase):')
+        if create_fixtures:
+            log.info('\tfixtures = ["%s"]' % fixture_file)
+        else:
+            log.info('\t#fixtures = ["%s"]' % app_name)
+
+        if verbosity > 0:
+            print "Inserting TestMaker logging server..."
+        settings.MIDDLEWARE_CLASSES += ('test_utils.middleware.testmaker.TestMakerMiddleware',)
+        
+        from django.core.management import call_command
+        try:
+            call_command('runserver', addrport=addrport, use_reloader=False)
+        except SystemExit:
+            if create_fixtures:
+                make_fixtures(fixture_file, app)
+        
+
+def make_fixtures(fixturefile, app):
+    print "Creating fixture at " + fixturefile
+    print "Not implemented"
---
+++ test_utils/middleware/testmaker.py
@@ -0,0 +1,57 @@
+from django.conf import settings
+from django.test import Client
+from django.test.utils import setup_test_environment
+import logging, re
+
+log = logging.getLogger('testmaker')
+print "Loaded Testmaker Middleware"
+
+#Remove at your own peril
+debug = getattr(settings, 'DEBUG', False)
+if not debug:
+   print "THIS CODE IS NOT MEANT FOR USE IN PRODUCTION"
+   #return
+
+class TestMakerMiddleware(object):
+   def process_request(self, request):
+      if 'test_client_true' not in request.REQUEST:
+         log_request(request)
+         if request.method.lower() == "get":
+            setup_test_environment()
+            c = Client()
+            getdict = request.GET.copy()
+            getdict['test_client_true'] = 'yes' #avoid recursion
+            r = c.get(request.path, getdict)
+            log_status(request.path, r)
+            if r.context:
+               con = get_user_context(r.context) 
+               output_user_context(con)
+            
+def log_request(request):
+   log.info('\n\tdef %s(self): ' % 'test_path')
+   method = request.method.lower()
+   request_str = "'%s', {" % request.path
+   for dict in request.REQUEST.dicts:
+      for arg in dict:
+         request_str += "'%s': '%s', " % arg, request.REQUEST[arg]
+   request_str += "}"
+   log.info("\t\tr = c.%s(%s)" % (method, request_str))
+
+def log_status(path, request):
+   log.info("\t\tself.assertEqual(r.status_code, %s)" % request.status_code)
+
+def get_user_context(context_list):
+   if isinstance(context_list, list):
+      context_list = context_list[-1] #Last context rendered
+      ret = context_list.dicts[-1]
+      if ret == {}:
+         ret = context_list.dicts[0]
+      return ret
+   else:
+      return context_list
+   
+def output_user_context(context):
+    for var in context:
+        if not re.search("0x\w+", unicode(context[var])): #Avoid memory addy's which will change.
+            log.info(u"\t\tself.assertEqual(unicode(r.context[-1]['%s']), u"""%s""")" % (var, unicode(context[var])))
+   
