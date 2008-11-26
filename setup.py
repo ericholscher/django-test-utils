88620ddc11c71840b6e23109028ee103f1add027
Eric Holscher <eric@ericholscher.com>
2008-11-26 11:03:15 -0600

    Made setup.py actually for this project :)

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "django-testmaker",
-    version = "0.2a3",
+    name = "django-test-utils",
+    version = "0.2",
     packages = find_packages(),
     author = "Eric Holscher",
     author_email = "eric@ericholscher.com",
-    description = "A package to help automate creation of testing in Django",
-    url = "http://code.google.com/p/django-testmaker/",
+    description = "A package to help testing in Django",
+    url = "http://github.com/ericholscher/django-test-utils/tree/master",
     include_package_data = True
 )
