a48ef853ae4cbf77c2a8729c673bdb23fd75e4b6
Eric Holscher <eric@ericholscher.com>
2009-04-12 19:21:36 -0500

    Committed the beginning of the unified diff output for assertEquals.

---
+++ test_utils/assertions.py
@@ -0,0 +1,36 @@
+"""
+Code originally from: http://www.aminus.org/blogs/index.php/2009/01/09/assertnodiff
+"""
+
+import difflib
+from pprint import pformat
+
+
+class DiffTestCaseMixin(object):
+
+    def get_diff_msg(self, first, second,
+                     fromfile='First', tofile='Second'):
+        """Return a unified diff between first and second."""
+        # Force inputs to iterables for diffing.
+        # use pformat instead of str or repr to output dicts and such
+        # in a stable order for comparison.
+        if isinstance(first, (tuple, list, dict)):
+            first = [pformat(d) for d in first]
+        else:
+            first = [pformat(first)]
+
+        if isinstance(second, (tuple, list, dict)):
+            second = [pformat(d) for d in second]
+        else:
+            second = [pformat(second)]
+
+        diff = difflib.unified_diff(
+            first, second, fromfile=fromfile, tofile=tofile)
+        # Add line endings.
+        return ''.join([d + '\n' for d in diff])
+
+    def failIfDiff(self, first, second, fromfile='First', tofile='Second'):
+        """If not first == second, fail with a unified diff."""
+        if not first == second:
+            msg = self.get_diff_msg(first, second, fromfile, tofile)
+            raise self.failureException, msg
