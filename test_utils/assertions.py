2124f859a74d4a3e00524af62d1122796804f048
Eric Holscher <eric@ericholscher.com>
2009-05-14 08:47:53 -0500

    Make failIfDiff work with dict keys and values.

--- test_utils/assertions.py
+++ test_utils/assertions.py
@@ -14,20 +14,24 @@ class DiffTestCaseMixin(object):
         # Force inputs to iterables for diffing.
         # use pformat instead of str or repr to output dicts and such
         # in a stable order for comparison.
-        if isinstance(first, (tuple, list, dict)):
+        if isinstance(first, (tuple, list)):
             first = [pformat(d) for d in first]
+        elif isinstance(first, dict):
+            first = ["%s:%s" % (pformat(key), pformat(val)) for key,val in first.iteritems()]
         else:
             first = [pformat(first)]
 
-        if isinstance(second, (tuple, list, dict)):
+        if isinstance(second, (tuple, list)):
             second = [pformat(d) for d in second]
+        elif isinstance(second, dict):
+            second = ["%s:%s" % (pformat(key), pformat(val)) for key,val in second.iteritems()]
         else:
             second = [pformat(second)]
 
         diff = difflib.unified_diff(
             first, second, fromfile=fromfile, tofile=tofile)
         # Add line endings.
-        return ''.join([d + '\n' for d in diff])
+        return '\n' + ''.join([d + '\n' for d in diff])
 
     def failIfDiff(self, first, second, fromfile='First', tofile='Second'):
         """If not first == second, fail with a unified diff."""
