8ab2fe65ce83140b96f9bb2d9c8c1e242dea463d
Eric Holscher <eric@ericholscher.com>
2009-06-11 22:32:51 -0500

    Added auth to the test settings for south demo.

--- test_project/settings.py
+++ test_project/settings.py
@@ -67,7 +67,7 @@ TEMPLATE_DIRS = (
 )
 
 INSTALLED_APPS = (
-    #'django.contrib.auth',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
