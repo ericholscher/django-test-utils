c49f577af48797dab9c7d04edc4ccc5a5b4f8f05
Eric Holscher <eric@ericholscher.com>
2009-07-02 00:18:27 -0500

    Replay stuff actually almost works, still a hack, but I'll get around to making it nice eventually.

--- test_utils/testmaker/replay.py
+++ test_utils/testmaker/replay.py
@@ -1,25 +1,43 @@
 import re
 import cPickle as pickle
+from django.utils import simplejson as json
+from test_utils.testmaker import setup_logging
+from test_utils.testmaker.base_processor import Processer
 
-f = open('/Users/ericholscher/lib/test/playground/tests/playground_testdata.pickle')
+in_file = '/Users/ericholscher/lib/artocrats/pieces/tests/pieces_testdata.pickle'
+f = open(in_file)
 
 buffer = []
 
 req_re = re.compile('---REQUEST_BREAK---')
 res_re = re.compile('---RESPONSE_BREAK---')
 
+processor = Processer()
+
+setup_logging('/Users/ericholscher/lib/artocrats/pieces/tests/redo.py', '/dev/null')
+
+serial_obj = json
+
+class Request(dict):
+    'Mocking a dict to allow attribute access'
+    def __getattr__(self, name):
+        return self[name]
+
 for line in f.readlines():
     if req_re.search(line):
         #process request
         to_pickle = ''.join(buffer)
-        obj = pickle.loads(to_pickle)
-        print obj['path'], obj['time']
+        request = Request(serial_obj.loads(to_pickle))
+        import ipdb; ipdb.set_trace()
+        processor.log_request(request)
+        print request['path'], request['time']
         buffer = []
     elif res_re.search(line):
         #process response
         to_pickle = ''.join(buffer)
-        obj = pickle.loads(to_pickle)
-        print obj['status_code'], obj['time']
+        response = Request(serial_obj.loads(to_pickle))
+        self.processer.process_response(request, response)
+        print response['status_code'], response['time']
         buffer = []
     else:
         buffer.append(line)
