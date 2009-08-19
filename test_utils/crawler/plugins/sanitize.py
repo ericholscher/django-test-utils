ae343ea630b08b4367f5d5bfb55d8609f9303344
Michael Thornhill <michael.thornhill@gmail.com>
2009-08-19 22:05:08 +0800

    recorded broken html to a file for analysis
    Signed-off-by: Eric Holscher <eric@ericholscher.com>

--- test_utils/crawler/plugins/sanitize.py
+++ test_utils/crawler/plugins/sanitize.py
@@ -5,6 +5,15 @@ class Sanitize(Plugin):
     "Make sure your response is good"
 
     def post_request(self, sender, **kwargs):
-        soup = BeautifulSoup(kwargs['response'].content)
-        if soup.find(text='&lt;') or soup.find(text='&gt;'):
-            print "%s has dirty html" % url
+
+        try:
+            soup = BeautifulSoup(kwargs['response'].content)
+            if soup.find(text='&lt;') or soup.find(text='&gt;'):
+                print "%s has dirty html" % url
+        except Exception, e:
+            fo = open("temp.html", 'w')
+            fo.write(kwargs['response'].content)
+            fo.close()
+            print 'bad html in file temp.html'
+            raise e
+                     
