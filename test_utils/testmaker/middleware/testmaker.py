8b96634e9da70a1828bc7250d8606c49b1d3c40e
Eric Holscher <eric@ericholscher.com>
2009-08-24 00:16:06 -0500

    Use the new interface in the middleware.

--- test_utils/testmaker/middleware/testmaker.py
+++ test_utils/testmaker/middleware/testmaker.py
@@ -2,8 +2,8 @@ from django.conf import settings
 from django.test import Client
 from django.test.utils import setup_test_environment
 
-from test_utils.testmaker.base_processor import Processer
-from test_utils.testmaker.base_serializer import Serializer
+from test_utils.testmaker import processors
+from test_utils.testmaker import serializers
 
 #Remove at your own peril.
 #Thar be sharks in these waters.
@@ -20,8 +20,8 @@ class TestMakerMiddleware(object):
         Serializers will be pluggable and allow for custom recording.
         Processers will process the serializations into test formats.
         """
-        self.serializer = Serializer()
-        self.processer = Processer()
+        self.serializer = serializers.get_serializer('pickle')
+        self.processer = processors.get_processor('django')
 
     def process_request(self, request):
         """
@@ -32,12 +32,13 @@ class TestMakerMiddleware(object):
         #This is request.REQUEST to catch POST and GET
         if 'test_client_true' not in request.REQUEST:
             self.serializer.save_request(request)
-            self.processer.process_request(request)
+            self.processer.save_request(request)
+            #We only want to re-run the request on idempotent requests
             if request.method.lower() == "get":
                 setup_test_environment()
                 c = Client(REMOTE_ADDR='127.0.0.1')
                 getdict = request.GET.copy()
                 getdict['test_client_true'] = 'yes' #avoid recursion
                 response = c.get(request.path, getdict)
-                self.serializer.save_response(request.path, response)
-                self.processer.process_response(request, response)
+                self.serializer.save_response(request, response)
+                self.processer.save_response(request, response)
