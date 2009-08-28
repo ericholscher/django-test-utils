c7b2ba7dcffed5112bae6ac6f63517b299f23279
Eric Holscher <eric@ericholscher.com>
2009-08-27 23:03:40 -0500

    Update management command to work with the new structure.

--- test_utils/management/commands/testmaker.py
+++ test_utils/management/commands/testmaker.py
@@ -2,32 +2,51 @@ from optparse import make_option
 import logging, os
 from os import path
 
-from django.core.management.base import AppCommand, CommandError
+from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 from django.core.management import call_command
+from django.db import models
 
 from test_utils.testmaker import Testmaker
 
-class Command(AppCommand):
-    option_list = AppCommand.option_list + (
-        make_option('-f', '--fixture', action='store_true', dest='fixture',
-            help='Pass -f to create a fixture for the data.'),
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('-a', '--app', action='store', dest='application',
+            default=None, help='The name of the application (in the current \
+                    directory) to output data to. (defaults to currect directory)'),
+        make_option('-l', '--logdir', action='store', dest='logdirectory',
+            default=os.getcwd(), help='Directory to send tests and fixtures to. \
+            (defaults to currect directory)'),
+        make_option('-x', '--loud', action='store', dest='verbosity', default='1',
+            type='choice', choices=['0', '1', '2'],
+            help='Verbosity level; 0=minimal output, 1=normal output, 2=all output'),
+        make_option('-f', '--fixture', action='store_true', dest='fixture', default=False,
+            help='Pass -f to not create a fixture for the data.'),
         make_option('--format', default='json', dest='format',
             help='Specifies the output serialization format for fixtures.'),
-        make_option('--addrport', action='store', dest='addrport',
-            type='string', help='port number or ipaddr:port to run the server on'),
     )
 
-    help = 'Runs the test server with the testmaker output enabled for a specific app'
-    args = '[app to test]'
+    help = 'Runs the test server with the testmaker output enabled'
+    args = '[server:port]'
 
-    def handle_app(self, app, **options):
+    def handle(self, addrport='', *args, **options):
+
+        app = options.get("application")
         verbosity = int(options.get('verbosity', 1))
         create_fixtures = options.get('fixture', False)
-        format = options.get('format', 'xml')
-        addrport = options.get('addrport', '')
+        logdir = options.get('logdirectory')
+        fixture_format = options.get('format', 'xml')
 
-        testmaker = Testmaker(app, verbosity, create_fixtures, format, addrport)
+        if app:
+            app = models.get_app(app_name)
+
+        if not app:
+            #Don't serialize the whole DB :)
+            create_fixtures = False
+
+        testmaker = Testmaker(app, verbosity, create_fixtures, fixture_format, addrport)
+        testmaker.prepare(insert_middleware=True)
         try:
             call_command('runserver', addrport=addrport, use_reloader=False)
         except SystemExit:
