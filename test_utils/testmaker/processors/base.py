12726962b1e7d23d9b0f89df9f0c7789cce8499f
Peter van Kampen <pterk@datatailors.com>
2010-01-24 19:16:02 +0800

    Fix logging the context (solves a.o. 'dict objects are unhashable', also some refactoring)

--- test_utils/testmaker/processors/base.py
+++ test_utils/testmaker/processors/base.py
@@ -1,6 +1,25 @@
-from django.template.defaultfilters import slugify as base_slugify
 import logging
 import re
+import time
+
+from django.template.defaultfilters import slugify as base_slugify
+from django.template import Template, Context
+from django.utils.encoding import force_unicode
+from django.utils.safestring import mark_safe
+
+from test_utils.templatetags import TemplateParser
+
+TEST_TEMPLATE = 'Override in Subclass'
+STATUS_TEMPLATE = 'Override in Subclass'
+CONTEXT_TEMPLATE = 'Override in Subclass'
+#DISCARD_CONTEXT_KEYS = ('LANGUAGES',)
+DISCARD_CONTEXT_KEYS = []
+
+def safe_dict(dict):
+    new_dic = {}
+    for key,val in dict.iteritems():
+        new_dic[key] = mark_safe(val)
+    return new_dic
 
 def slugify(toslug):
     """
@@ -26,10 +45,91 @@ class Processer(object):
         raise NotImplementedError
 
     def save_request(self, request):
-        raise NotImplementedError
+        """ Actually write the request out to a file """
+        if self.shall_we_proceed(request):
+            self._log_request(request)
 
     def process_response(self, request, response):
         raise NotImplementedError
 
     def save_response(self, request, response):
+        if self.shall_we_proceed(request):
+            self._log_status(response)
+            if response.context and response.status_code != 404:
+                self._log_context(response.context)
+                #This is where template tag outputting would go
+                #Turned off until it gets betterer
+                """
+                parser = TemplateParser(response.template[0], context)
+                parser.parse()
+                parser.create_tests()
+                """
+
+    def _get_template(self, templatename):
+        """Should be implemented in subclass"""
         raise NotImplementedError
+
+    def _log_request(self, request):
+        method = request.method.lower()
+        request_str = "'%s', {" % request.path
+        for dikt in request.REQUEST.dicts:
+            for arg in dikt:
+                request_str += "'%s': '%s', " % (arg, request.REQUEST[arg])
+        request_str += "}"
+
+        template = Template(self._get_template('test'))
+        context = {
+            'path': slugify(request.path),
+            'time': slugify(time.time()),
+            'method': method,
+            'request_str': request_str,
+        }
+        context = Context(safe_dict(context))
+        self.log.info(template.render(context))
+
+    def _log_status(self, response):
+        template = Template(self._get_template('status'))
+        context = {
+            'status_code': response.status_code,
+        }
+        if response.status_code in [301, 302]:
+            context['location'] = response['Location']
+        context = Context(safe_dict(context))
+        self.log.info(template.render(context))
+
+    def _get_context_keys(self, context):
+        """Get the keys from the contexts(list) """
+        keys = []
+        for d in context.dicts:
+            if isinstance(d, Context):
+                return self._get_context_keys(d)
+            keys += d.keys()
+        return keys
+
+    def _log_context(self, context):
+        template = Template(self._get_template('context'))
+        if isinstance(context, list):
+            keys = []
+            for c in context:
+                keys += self._get_context_keys(c)
+        else:
+            keys += self._get_context_keys(context)
+        keys = set(keys)
+
+        # Skip some keys 
+        for discardkey in DISCARD_CONTEXT_KEYS:
+            keys.discard(discardkey)
+
+        for key in keys:
+            val = force_unicode(context[key])
+            con = {
+                'key': key,
+                'value': val,
+            }
+            con = Context(safe_dict(con))
+            try:
+                #Avoid memory addy's which will change.
+                if not re.search("0x\w+", val):
+                    self.log.info(template.render(con))
+            except UnicodeDecodeError, e:
+                pass
