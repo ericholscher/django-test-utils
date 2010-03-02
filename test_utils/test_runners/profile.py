5ec1f9d7098992275f028f4fac73692b39486770
Eric Holscher <eric@ericholscher.com>
2010-03-01 22:31:35 -0600

    Corrent profiling test runner docstring :x

--- test_utils/test_runners/profile.py
+++ test_utils/test_runners/profile.py
@@ -5,11 +5,11 @@ from django.test.simple import run_tests as django_test_runner
 def run_tests(test_labels, verbosity=1, interactive=True,
         extra_tests=[], nodatabase=False):
     """
-    Test runner which displays a code coverage report at the end of the
-    run.
+    Test runner which displays basic profile data.
+    Needs some improvement, mostly here for Continuous Integration purposes.
     """
     print "Using profiling test runner"
     cProfile.runctx("django_test_runner(test_labels, verbosity, interactive, extra_tests)", globals(), locals(), filename="django_tests.profile")
     stats = pstats.Stats('django_tests.profile')
-    stats.strip_dirs().sort_stats('time').print_stats(20)
+    stats.strip_dirs().sort_stats('time').print_stats(30)
     return True
